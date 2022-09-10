a8fe284d349fc024ebaafdaf7ffc32be1943584c
Beloin Sena <beloin.rodrigues@gmail.com>
2022-09-10 00:56:11 -0300

    Played a little bit

--- src/main/java/com/beloin/javafxbeloin/HelloController.java
+++ src/main/java/com/beloin/javafxbeloin/HelloController.java
@@ -1,14 +1,19 @@
 package com.beloin.javafxbeloin;
 
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
 import javafx.scene.control.Label;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderStroke;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
+    private Integer num = 0;
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        num++;
+        welcomeText.setText("Welcome to JavaFX Application! " + num);
     }
 }
